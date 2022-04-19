e1039636c2b9feb56d3d32dfa94562cbad16d095
Elkanah <elkanahawesome@gmail.com>
2022-04-19 06:03:08 -0500

    made buttons display on screen added a clear functionality

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
+let displayCurrentValue = "";
 let currentValue = 0;
+let pressedHowManyTimes = 0;
 //do calculations
 //store 
 function add(a, b) {
+    
     return a+b;
 }
 
@@ -17,9 +20,13 @@ function divide(a, b) {
     return a/b;
 }
 
-/* Create a new function operate that takes an operator and 2 numbers 
-and then calls one of the above functions on the numbers. */
+function clear() {
+    displayElement.innerHTML = 0;
+    currentValue = 0;
 
+}
+
+//determine what symbol then operate
 function operate(symbol, a, b) {
     switch (`${symbol}`) {
         case "+":
@@ -39,4 +46,30 @@ function operate(symbol, a, b) {
             break; 
 
         }
+}
+
+//select display element and number buttons
+let displayElement = document.querySelector(".display");
+let numberButtons = document.querySelectorAll(".buttonNumbers");
+
+//listen to when is clicked
+numberButtons.forEach(buttons => {
+    buttons.addEventListener("click", changeDisplay);
+});
+
+document.querySelector(".clear").addEventListener("click", clear);
+
+
+//use another to store calculations
+function changeDisplay(num) {
+    //check howmanytimespressed to remove default val 0
+    if (pressedHowManyTimes == 0) displayElement.innerText = "";
+    pressedHowManyTimes++
+    
+    //get the number that was pressed
+    //use a variable and add numbers together in string
+    displayCurrentValue += num.explicitOriginalTarget.dataset.numbers;
+    
+    //then change the display contents
+    displayElement.innerText = displayCurrentValue;
 }
